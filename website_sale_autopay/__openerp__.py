e8335d884b3cd9ebdfea59d9443e95d71bc8d858
ilyasProgrammer <rakhimkulov@it-projects.info>
2016-08-31 12:23:01 +0500

    [DOC] vers, log, descr

--- website_sale_autopay/__openerp__.py
+++ website_sale_autopay/__openerp__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 {
     "name": """Autopay in eCommerce""",
-    "summary": """Auto invoice creating on payment""",
+    "summary": """Auto invoice creation on payment""",
     "category": "eCommerce",
     "images": [],
-    "version": "1.0.0",
+    "version": "2.0.0",
 
     "author": "IT-Projects LLC",
     "website": "https://it-projects.info",
---
+++ website_sale_autopay/doc/changelog.rst
@@ -0,0 +1,15 @@
+.. _changelog:
+
+Changelog
+=========
+
+`2.0.0`
+-------
+
+- added multi company support
+
+
+`1.0.0`
+-------
+
+- init version
